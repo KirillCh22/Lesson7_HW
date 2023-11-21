2fa8acef18cbfafa88658b99dd9d584387318b51
Kirill <147234461+KirillCh22@users.noreply.github.com>
2023-11-21 21:50:27 +0300

    Completed HomeWork.

---
+++ Task2/Program.cs
@@ -0,0 +1,33 @@
+﻿                        //  Задача 2
+//  Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+//  Даны два неотрицательных числа m и n.
+
+//                  * m = 2, n = 3  ->  A(m, n) = 29
+
+
+//===============  ФУНКЦИЯ, КОТОРАЯ РАСЧИТЫВАЕТ ФУНКЦИЮ АНКЕРМАНА  ========================
+//===============  И ВОЗВРАЩАЕТ ЗНАЧЕНИЕ (ИСПОЛЬЗУЕТСЯ РЕКУРСИЯ)  =========================
+int Ackerman(int n, int m)
+{
+    if( n == 0) return m + 1;
+    else if (m == 0) return Ackerman(n - 1, 1);
+    else return Ackerman(n - 1, Ackerman(n, m - 1));
+}
+
+
+//===============  ФУНКЦИЯ, КОТОРАЯ УПРАВЛЯЕТ ВСЕЙ ПРОГРАММОЙ  ===============================
+void Main()
+{
+    Console.Write("Введите первое число (n): ");
+    int n = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Введите второе число (m): ");
+    int m = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine();
+    
+    int total = Ackerman(n, m);
+    Console.Write($"n = {n}, m = {m} -> A(n, m) = {total}");
+}
+
+Main();
---
+++ Task3/Program.cs
@@ -0,0 +1,67 @@
+﻿//                                   Задача 3
+//         Задайте произвольный массив. Выведете его элементы, начиная с конца. 
+//          Использовать рекурсию, не использовать циклы.
+
+//              * [1, 2, 5, 0, 10, 34]  ->  34, 10, 0, 5, 2, 1
+
+
+
+//===================  ФУНКЦИЯ ПО СОЗДАНИЮ МАССИВА  ==================
+int[] GenerateArray()
+{
+    string[] str;
+    Console.WriteLine("Введите числа через , :");
+    str = Console.ReadLine().Split(",");
+
+    int[] numbers = new int[str.Length];
+    for(int i = 0; i < str.Length; i++)
+    {
+        numbers[i] = Convert.ToInt32(str[i]);
+    }
+
+    return numbers;
+}
+
+
+//==================  ФУНКЦИЯ ВЫВОДА МАССИВА НА ЭКРАН  =======================
+void PrintArray(int[] array)
+{
+    Console.WriteLine();
+    Console.Write("Изначальный Массив: [");
+    for(int i = 0; i < array.Length; i++)
+    {
+        if (i == array.Length - 1) Console.Write(array[i] + "]");
+        else Console.Write(array[i] + ", ");
+    }
+
+}
+
+//==============  ФУНКЦИЯ, КОТОРАЯ ВЫВОДИТ МАССИВ В ОБРАТНОМ ПОРЯДКЕ (ИСПОЛЬЗУЕТСЯ РЕКУРСИЯ) ==================
+void NewGenerateArray(int[] array, int len_array)
+{
+    if(len_array < 0) return;
+    else
+    {
+        Console.Write(array[len_array] + " ");
+    
+        NewGenerateArray(array, len_array - 1);
+
+    }
+}
+
+//===============  ФУНКЦИЯ, КОТОРАЯ УПРАВЛЯЕТ ВСЕЙ ПРОГРАММОЙ  ===============================
+void Main()
+{
+    int[] array = GenerateArray();
+    PrintArray(array);
+
+    int len_array = array.Length - 1;
+    Console.WriteLine();
+
+    Console.Write("Обратный массив: ");
+    NewGenerateArray(array, len_array);
+
+}
+
+Main();
+
